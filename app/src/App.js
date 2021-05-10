44e1eee4e48bc5aec19705f8ef2e89657ff3ef65
ClementLegouest <clement.legouest@gmail.com>
2021-05-10 16:43:14 +0200

    moving header from app.js to new component

--- app/src/App.js
+++ app/src/App.js
@@ -2,17 +2,12 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './router/Routes';
 import Navigation from './router/Nav';
-import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      {/* TODO 
-          Create a header component
-      */}
-      <header className="App-header">
-        <h1>Dionysos</h1>
-      </header>
+      <Header />
       <main>
         <BrowserRouter>
           <Navigation />
