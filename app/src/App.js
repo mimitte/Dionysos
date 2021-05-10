5303c037eb6a07a2574f0e06fa5c747726f2bdae
ClementLegouest <clement.legouest@gmail.com>
2021-05-10 10:31:58 +0200

    basic application

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dionysos</h1>
+      </header>
+      <main>
+        <p>Contenu de l'application</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
