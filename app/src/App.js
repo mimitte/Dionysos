145e7937c2d545fe636d4085f36ad7a66f1ac73e
avofeno <avofeno@gmail.com>
2021-05-11 17:40:22 +0200

    ajout composant Dashboard

--- app/src/App.js
+++ app/src/App.js
@@ -3,11 +3,11 @@ import { BrowserRouter } from 'react-router-dom';
 import Routes from './router/Routes';
 import MenuHamberger from './components/MenuHamberger';
 
-
 function App() {
   return (
     <div className="App">
       <main>
+      
         <BrowserRouter>
           <MenuHamberger/>
           <Routes />
---
+++ app/src/components/dashboard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class dashboard extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+           
+        }
+    }
+        
+    render() {
+        return(
+            <>
+                <h2>voici mon dashboard</h2>
+               
+            </>
+        );
+    }
+}
+export default dashboard;
