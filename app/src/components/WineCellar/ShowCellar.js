12f06946a79bf32461f56a4da1e3634c94be8c7f
will <willy.jluan@free.fr>
2021-05-12 22:29:07 +0200

    cellar component

--- app/src/components/WineCellar/ShowCellar.js
+++ app/src/components/WineCellar/ShowCellar.js
@@ -1,10 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-function ShowCellar() {
+function ShowCellar({area, columns, rows}) {
+    // let containerArea = document.querySelector(`div[data-area=${area}]`);
+    let htmlRow = [];
+    let htmlColumns = [];
+  
+    for (let i = 0; i < columns; i++){
+        htmlColumns.push(
+            <div className="column">
+                <span className="index-X">{i + 1}</span>
+                <div className="contentBottle drop-zone" data-bottle={i + 1}>
+                </div>
+            </div>);
+    }
+    for (let j = 0; j < rows; j++){
+        htmlRow.push(
+            <div className="lineBottle" data-lineBottle={j+1} key={j}>
+                <div className="row">{j+1}</div>
+                    {htmlColumns}
+                </div>);
+    }
     return (
-        <div>
-            super
-        </div>
+       <>
+        {htmlRow}
+       </>
     )
 }
 
--- app/src/components/WineCellar/WineCellar.js
+++ app/src/components/WineCellar/WineCellar.js
@@ -68,7 +68,7 @@ const  bottlesWRed = [
         note:"Pessac Leognan",
         country:"France",
         contentRed:3,
-        location:[7,2]
+        location:[3,2]
     }
 ]
 const  bottlesPink = [
@@ -81,7 +81,7 @@ const  bottlesPink = [
         note:"Chateau De Sours, Rosé, ",
         country:"France",
         contentPink:2,
-        location:[1,1],
+        location:[1,1]
     },
     {
         uid:12,
@@ -109,49 +109,157 @@ const  bottlesPink = [
 
 const cellarObjet = {
     uid:1,
-    name:"",
-    description:"",
-    contentWhite: { // table liste bouteilles  avec id
-        id:1,
-        columns:7,//x  type d'oragisation de la zone des bouteilles
-        rows:5,//y
-        cellar:1,
-        bottles:bottlesWhite, //liste bouteille blanc
-    },
-    contentPink: {
-        id:2,
-        columns:7,
-        rows:5,
-        cellar:1,
-        bottles:bottlesPink,//liste bouteille blanc
-    },
-    contentRed: {
-        id:3,
-        columns:7,
-        rows:5,
-        cellar:1,
-        bottles:bottlesWRed,//liste bouteille blanc
-    }
+    name:"Cave1",
+    description:"super cave de bordeaux",
+    areaCellar :[
+        { // table liste bouteilles  avec id
+            area:"blanc",
+            id:1,
+            columns:7,//x  type d'oragisation de la zone des bouteilles
+            rows:5,//y
+            cellar:1,
+            bottles:bottlesWhite //liste bouteille blanc
+        },
+        {
+            id:2,
+            area:"rose",
+            columns:5,
+            rows:4,
+            cellar:1,
+            bottles:bottlesPink//liste bouteille blanc
+        },
+        {
+            id:3,
+            area:"rouge",
+            columns:7,
+            rows:5,
+            cellar:1,
+            bottles:bottlesWRed//liste bouteille blanc
+        }
+    ]
 };
 
 export default class WineCellar extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            cellar: {
+            cellar: [
                 cellarObjet,
-                TotalBottle:cellarObjet.contentPink.bottles.length + cellarObjet.contentRed.bottles.length + cellarObjet.contentWhite.bottles.length
+            ],
+            totalBottle : 0,
+
+        }
+    }
+    componentDidMount = () => {
+        let area = this.state.cellar[0].areaCellar;
+        let inserteBottle = area.map((elements) => {
+            let color = elements.area;
+            if(color === "blanc" && elements.length !== 0){
+                this.dispatchBottle(elements, color);
+            }
+            if(color === "rouge" && elements.length !== 0){
+                this.dispatchBottle(elements, color);
+            }
+            if(color === "rose" && elements.length != 0){
+                this.dispatchBottle(elements, color);
+            }
+        });
+    }
+
+    dispatchBottle(elements, color){
+        this.totalBottlecellar(parseInt(elements.bottles.length,10));
+        let loc  = elements.bottles.map((bottle) => {
+                let rowBottle = bottle.location[0];
+                let columnBottle = bottle.location[1];
+                let element = document.querySelector("[data-area='" + color + "'] [data-linebottle='" + rowBottle + "'] [data-bottle='" + columnBottle + "']");
+                if(!element)return;
+                let drag = document.createElement("div");
+                drag.classList.add("draggable");
+                drag.setAttribute("draggable","true");
+                drag.setAttribute("id","draggable-" + bottle.uid);
+                drag.addEventListener('dragstart',this.dragStart)
+                element.append(drag);
+                element.classList.remove("drop-zone");
+        });
+        if(loc){
+            let containers = document.querySelectorAll('.column');
+            for (const container of containers) {
+                container.addEventListener('dragover',this.dragOver);
+                container.addEventListener('dragenter',this.dragEnter);
+                container.addEventListener('dragleave',this.dragLeave);
+                container.addEventListener('drop',this.dragDrop);
             }
         }
     }
-    dragAndGropBottles = (valeur) => {
-        this.setState({search: valeur})
-      }
+
+
+    dragOver(e){
+        e.preventDefault();
+        if(e.target != undefined ){
+            if(e.target.classList.contains("contentBottle") && e.target.hasChildNodes()){
+                    e.target.classList.add("drop-zone");
+                    console.log(e);
+            }
+        }
+    }
+
+
+    dragEnter(e){
+        e.preventDefault();
+    }
+
+    dragLeave(e){
+        e.preventDefault();
+    }
+
+    dragStart(e){
+        e.dataTransfer.setData("text",e.target.id);
+    }
+    dragDrop(e){
+        e.preventDefault();
+            if(e.target.classList.contains("drop-zone")){
+                const data = e.dataTransfer.getData('text');
+                e.target.append(document.getElementById(data));
+                e.target.classList.remove("drop-zone");
+            }
+        // e.preventDefault();
+        // const bottle_id = e.dataTransfer.getData('id');
+        // console.log(bottle_id);
+    }
+
+    totalBottlecellar(b){
+        this.setState((state) => ({
+            totalBottle: state.totalBottle + b,
+         }));
+    }
+
+    creatAreaCellars = ({areaElements, index}) =>{
+        let color = areaElements.area;
+        return(
+            <React.Fragment>
+                <h3>Emplacement vin {color} </h3>
+                <div className="area" data-area={areaElements.area} >
+                    <ShowCellar area={color} columns={areaElements.columns} rows={areaElements.rows} key={index}/>
+                </div>
+            </React.Fragment>
+        );
+    }
+
+    areaCellars = (Zone,x,y = 1) => {
+
+        return <ShowCellar/>;
+    }
+
+
     render() {
+        let area = this.state.cellar[0].areaCellar;
         return (
             <>
                 <h2>Cave</h2>
-                <ShowCellar/>
+                <h3>Nombre de bouteilles total : {this.state.totalBottle }</h3>
+                <section id="areaCellars">
+                    {area.map((elements, index) =><this.creatAreaCellars areaElements={elements}  index={index} key={index}/>)}
+                </section>
             </>
         )
     }
