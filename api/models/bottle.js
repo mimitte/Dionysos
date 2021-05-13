1d9e142a524c8f7c5dd9f467945b3cbc7f4f7bb7
ClementLegouest <clement.legouest@gmail.com>
2021-05-13 18:28:04 +0200

    the bottle model

--- api/models/bottle.js
+++ api/models/bottle.js
@@ -6,10 +6,7 @@ const BottleSchema = mongoose.Schema({
   name: { type: String, required: true },
   year: { type: Number, required: true },
   color: { type: String, required: true },
-  // note: { type: String, required: false },
-  // vaultUid: { type: String, required: true },
-  // horizontalPosition: { type: Number, required: true },
-  // verticalPosition: { type: Number, required: true }
+  zoneUid: { type: String, required: true}
 });
 
 module.exports = mongoose.model('Bottle', BottleSchema);
