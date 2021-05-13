578e469558dfeb67a5305bf1de882c33d10fbb90
ClementLegouest <clement.legouest@gmail.com>
2021-05-13 18:15:01 +0200

    finish cellar model

--- api/models/cellar.js
+++ api/models/cellar.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const CellarSchema = mongoose.Schema({
     name: { type: String, required: true},
+    description: { type: String, required: true}
 });
 
 module.exports = mongoose.model('Cellar', CellarSchema);
