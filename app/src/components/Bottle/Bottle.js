03d4bde507e82a0cbf908768e7ed6daf2468c4f9
Denis Fortict <d.fortict@vivit.fr>
2021-05-15 10:27:03 +0200

    Bottle card better style

--- app/src/components/Bottle/Bottle.js
+++ app/src/components/Bottle/Bottle.js
@@ -8,16 +8,16 @@ class Bottle extends Component {
             region: "Bordeaux",
             name: "Petrus",
             year: "1970",
-            color: "rouge"
+            color: "blanc"
          }
     }
 
     headercolor() {
 
-        if (this.state.color == "rouge") {
+        if (this.state.color === "rouge") {
             return "background-red";
                 
-        } else if (this.state.color == "blanc") {
+        } else if (this.state.color === "blanc") {
             return "background-white";
         } else {
                 return "background-pink";
@@ -28,17 +28,15 @@ class Bottle extends Component {
         return (
             <React.Fragment>
                 {
-                    <div className="card bottlecard mb-3">
-                        <div className="card-header bg-transparent">
-                            <div className={this.headercolor()}>Country: {this.state.country}
-                            </div>
+                    <div className="card bottlecard  mb-3">
+                        <div className={this.headercolor()}>Country: {this.state.country}
                         </div>
                         <div className="card-body">
                             <h5 className="card-title">{this.state.name}</h5>
                             <p className="card-text">{this.state.year}</p>
                             <p className="card-text">{this.state.region}</p>
                         </div>
-                        <div className="card-footer bg-transparent">
+                        <div className="card-footer ">
                             <button className="btn btn-primary">Ok</button>
                             <button className="btn btn-warning">Update</button>
                             <button className="btn btn-danger">Delete</button>
