13148001eb47b6e182de773b921ad556b654062f
Denis Fortict <d.fortict@vivit.fr>
2021-06-09 15:56:01 +0200

    Zone Schema model userId

--- api/models/ZoneModel.js
+++ api/models/ZoneModel.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 const ZoneSchema = mongoose.Schema({
-    userId: { type: String, require: true},
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "UserId",
+        required: true
+    },
     name: { type: String, required: true},
     columns: { type: Number, required: true},
     rows: { type: Number, required: true},
